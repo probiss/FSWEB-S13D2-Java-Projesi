f4e9c67aed41ccbc035c21112e06837beb2b05ad
probiss <bulut.cihad@gmail.com>
2023-07-29 18:47:05 +0300

    Mission2 done.

---
+++ src/MissionsDay2/Mission2.java
@@ -0,0 +1,21 @@
+package MissionsDay2;
+
+public class Mission2 {
+    public static boolean isPerfectNumber(int num) {
+        if( num <= 0) {
+            return false;
+        }
+
+        int perfect = 0;
+
+        for(int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                perfect += i;
+            }
+        }
+        return  perfect == num;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(28));
+    }
+    }
