b9e1261582a9110c5827af9c70fdbbb5cea4d123
probiss <bulut.cihad@gmail.com>
2023-07-29 19:30:01 +0300

    Mission2 updated. Sayının yarısı alınıp hızdan kazanç

--- src/MissionsDay2/Mission2.java
+++ src/MissionsDay2/Mission2.java
@@ -8,7 +8,7 @@ public class Mission2 {
 
         int perfect = 0;
 
-        for(int i = 1; i < num; i++) {
+        for(int i = 1; i <= num/2 ; i++) {
             if (num % i == 0) {
                 perfect += i;
             }
