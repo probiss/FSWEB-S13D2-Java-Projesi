19ba49e6fb74f4d8d86c8a5a285905ffbfd4e8fd
probiss <bulut.cihad@gmail.com>
2023-07-29 18:23:40 +0300

    Mission1 done.

---
+++ src/MissionsDay2/Mission1.java
@@ -0,0 +1,27 @@
+package MissionsDay2;
+
+public class Mission1 {
+    public static boolean isPalindrome(int num){
+        if( num < 0 ){
+            return false;
+        }
+
+        int normalNum = num;
+        int reverse = 0;
+
+        while(num > 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+        return normalNum == reverse;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(55000055));
+    }
+
+    //StringBuilder'ın reverse() metodu sayesinde orijinal metni
+    // değiştirmeden kolayca tersine çevirebiliriz. Bu, metin
+    // işlemlerinde veya dizi işlemlerinde oldukça faydalı bir işlemdir.
+}
